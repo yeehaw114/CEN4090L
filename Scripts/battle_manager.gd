b44fc7c40e1060771fc9f04834073f4b9b2cfc78
Zachary Herman <zherman114@gmail.com>
2025-09-07 17:18:19 -0400

    Added buttons and ability to select buttons with arrow keys and z

--- Scripts/battle_manager.gd
+++ Scripts/battle_manager.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export var player: Character
 @export var enemy: Character
-
 var current_character: Character
 
+var game_over: bool = false
+
 func next_turn():
-	pass
+	if game_over:
+		return
