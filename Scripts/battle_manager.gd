ddca0c4b65d4a89c760d0f234c13c97ef36487ba
Zachary Herman <zherman114@gmail.com>
2025-09-07 15:17:54 -0400

    Added a battle manager scene

---
+++ Scripts/battle_manager.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var player: Character
+@export var enemy: Character
+
+var current_character: Character
+
+func next_turn():
+	pass
---
+++ Scripts/character.gd
@@ -0,0 +1,23 @@
+extends Node
+class_name Character
+
+@export_category('Stats')
+@export var health: int
+
+func begin_turn():
+	pass
+	
+func end_turn():
+	pass
+	
+func _process(delta: float) -> void:
+	pass
+	
+func take_damage():
+	pass
+	
+func heal():
+	pass
+	
+func cast_combat_action():
+	pass
