cc70844ecd61c7b42b7d13ac28f77d67f1b0de43
Zachary Herman <zherman114@gmail.com>
2025-09-13 13:06:08 -0400

    removed empty space in function

--- Scripts/ui/hand_area.gd
+++ Scripts/ui/hand_area.gd
@@ -17,24 +17,18 @@ func update_cards() -> void:
 	var cards := get_child_count()
 	var all_cards_size := Card.SIZE.x * cards + x_sep * (cards-1)
 	var final_x_sep := x_sep
-	
 	if all_cards_size > size.x:
 		final_x_sep = (size.x - Card.SIZE.x * cards) / (cards-1)
 		all_cards_size = size.x
-		
 	var offset := (size.x - all_cards_size) / 2
-	
 	for i in cards:
 		var card := get_child(i)
 		var y_multiplier := hand_curve.sample(1.0 / (cards-1) * i)
 		var rot_multiplier := rotation_curve.sample(1.0 / (cards-1) * i)
-		
 		if cards == 1:
 			y_multiplier = 0.0
 			rot_multiplier = 0.0
-			
 		var final_x: float = offset + Card.SIZE.x * i + final_x_sep * i
 		var final_y: float = y_min + y_max * y_multiplier
-		
 		card.position = Vector2(final_x,final_y)
 		card.rotation_degrees = max_rotation_degress * rot_multiplier
