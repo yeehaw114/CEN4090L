3ab9f75b770bc3dd400f5e36b16080f1023ca125
Zachary Herman <zherman114@gmail.com>
2025-10-25 09:58:51 -0400

    shop UI now sits in the middle of the screen regardless of the camera position

--- Scripts/lobby.gd
+++ Scripts/lobby.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
+@onready var shop_ui: Control = $CanvasLayer/ShopUI
 
 
 func _on_shop_trigger_body_entered(body: Node2D) -> void:
 	print("Triggered by: ", body.name)
 	if body.name == "Player":
-		$ShopUI.visible = true 
+		shop_ui.visible = true 
 
 
 func _on_shop_trigger_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		$ShopUI.visible = false 
+		shop_ui.visible = false 
--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -15,6 +15,7 @@ const SPEED = 200.0
 var last_facing = "down"
 var locked_direction = Vector2.ZERO
 var first_input_locked = false
+var able_to_move := true
 
 func _ready() -> void:
 	limit_camera(tilemap)
@@ -28,6 +29,8 @@ func _physics_process(_delta):
 	update_animation()
 
 func handle_movement_input():
+	if !able_to_move:
+		return
 	# If no direction is locked, check for new input
 	if not first_input_locked:
 		if Input.is_action_pressed("ui_up"):
@@ -103,3 +106,8 @@ func limit_camera(tilemap: TileMap):
 		camera_2d.limit_bottom = int(map_max.y + offset_bottom)
 	else:
 		push_warning("TileMap not found: %s" % str(tilemap))
+
+
+func _on_shop_trigger_body_entered(body: Node2D) -> void:
+	#Stop the player from moving
+	pass
