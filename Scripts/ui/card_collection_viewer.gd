f4b822036d3f02207b6817bf7e062d6be6f2e6c7
Zachary Herman <zherman114@gmail.com>
2025-10-26 16:46:39 -0400

    created CardCollectionViewer and created folder to store the final cards

---
+++ Scripts/ui/card_collection_viewer.gd
@@ -0,0 +1,10 @@
+extends Control
+
+var card_file_path := "res://Assets/Resources/cards/final_cards/"
+
+func populate_grid():
+	#create a new card scene
+	#apply stats to card
+	#place card in scene
+	pass
+	
