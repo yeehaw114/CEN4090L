68295a12cdf073ffc945e538a2b17b35124dbcb6
Olivia Coats <omc22a@fsu.edu>
2025-10-27 18:25:22 -0400

    made cards fit into card slots

--- Inventory/inv_ui.gd
+++ Inventory/inv_ui.gd
@@ -1,16 +1,19 @@
 extends Control
 
 @onready var inv: Inv = preload("res://Inventory/playerinventory.tres")
+@onready var card_test: CardResource = preload("res://Assets/Resources/cards/final_cards/defend_card.tres")
+@onready var card_scence: PackedScene = preload("res://Scenes/card.tscn")
+
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 @export var card_scene: PackedScene = preload("res://Scenes/card.tscn")
-@onready var grid_container: GridContainer = $NinePatchRect/GridContainer
+@onready var panel_container: PanelContainer = $NinePatchRect/MarginContainer/PanelContainer
 
 var is_open = false
 
 func _ready():
 	var test_card: CardResource = preload("res://Inventory/Items/testcard.tres")
-	add_card_to_inventory(test_card)
+	call_deferred("add_card_to_inventory", test_card)
 	update_slots()
 	close()
 
@@ -19,7 +22,7 @@ func update_slots():
 		slots[i].update(inv.slots[i])
 
 func _process(delta):
-	if Input.is_action_just_pressed("i"):
+	if Input.is_action_just_pressed("open inventory"):
 		if is_open:
 			close()
 		else:
@@ -33,10 +36,10 @@ func close():
 	visible = false
 	is_open = false
 
-func add_card_to_inventory(card_stat: CardResource) -> void:
-	var new_card = card_scene.instantiate()
-	new_card.card_stats = card_stat
-	new_card.is_able_to_be_selected = false
-	grid_container.add_child(new_card)
-	inv.insert(card_stat)
-	update_slots()  
+#func add_card_to_inventory(card_stat: CardResource) -> void:
+	#var new_card = card_scene.instantiate()
+	#new_card.card_stats = card_stat
+	#new_card.is_able_to_be_selected = false
+	#panel_container.add_child(new_card)
+	#inv.insert(card_stat)
+	#update_slots()  
--- Inventory/inv_ui_slot.gd
+++ Inventory/inv_ui_slot.gd
@@ -1,6 +1,12 @@
 extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+@onready var card_scene: PackedScene = preload("res://Scenes/card.tscn")
+@onready var panel_container: PanelContainer = $NinePatchRect/MarginContainer/PanelContainer
+@onready var card_test: CardResource = preload("res://Assets/Resources/cards/final_cards/defend_card.tres")
+
+func _ready():
+	add_card_to_inventory(card_test)
 
 func update(slot: InvSlot):
 	if !slot.card:
@@ -13,3 +19,10 @@ func update(slot: InvSlot):
 		else:
 			# Card exists but has no image
 			item_visual.visible = false
+			
+func add_card_to_inventory(card_stat: CardResource) -> void:
+	var new_card = card_scene.instantiate()
+	new_card.card_stats = card_stat
+	new_card.call_deferred("set_values")
+	new_card.is_able_to_be_selected = false
+	panel_container.add_child(new_card)
