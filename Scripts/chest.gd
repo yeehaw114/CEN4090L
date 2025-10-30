45a7aa7f9a59d929341d999e5ba3229dc51e5482
Zachary Herman <zherman114@gmail.com>
2025-10-29 20:49:59 -0400

    chest now popups coins earned

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var coins := 0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var coins_popup_position: Marker2D = $CoinsPopupPosition
 
 const closed_region = Rect2(0,32,16,16)
 const open_region = Rect2(0,0,16,32)
@@ -14,6 +15,7 @@ func open():
 		return
 	chest_opened = true
 	GameState.coins_current += coins
+	coins_popup_position.popup(coins)
 	print('COINS NEW VALUE: '+str(GameState.coins_current))
 	sprite_2d.region_rect = open_region
 	sprite_2d.offset = Vector2(0,-16)
